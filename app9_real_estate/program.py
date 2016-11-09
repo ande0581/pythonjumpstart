b2ba4d874718b71663b122c97c378892c216820e
janderson <jeffrey.d.anderson@gmail.com>
2016-11-08 18:26:01 -0600

    update load file to put data into class Purchase

--- app9_real_estate/program.py
+++ app9_real_estate/program.py
@@ -1,6 +1,8 @@
 import os
 import csv
 
+from app9_real_estate.data_types import Purchase
+
 
 def main():
     print_header()
@@ -22,14 +24,17 @@ def get_data_file():
 
 
 def load_file(filename):
-
     with open(filename, 'r', encoding='utf-8') as fin:
 
         reader = csv.DictReader(fin)  # this is pretty awesome
+        purchases = []
         for row in reader:
-            print(row)
+            # we don't need to make instance [p = Purchase()] first now that it has a @staticmethod in class
+            p = Purchase.create_from_dict(row)
+            purchases.append(p)
 
-    return []
+        # print(purchases[0].__dict__)
+        return purchases
 
 
 def query_data(data):
